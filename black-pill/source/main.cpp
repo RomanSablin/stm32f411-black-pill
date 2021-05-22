24bdd05f9fe8d1ab9e1baec68b7c04deb9fd9a0e
r.sablin <r.sablin@csort.ru>
2021-05-23 05:00:26 +0700

    build project

--- black-pill/source/main.cpp
+++ black-pill/source/main.cpp
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include "Board.h"
+#include "Uart.h"
 
 int main(int argc, char **argv)
 {
+	CBoard::Init();
+	CUart uart(USART1);
+	uart.Init();
+
+	for (;;)
+	{
+		uart.Send((uint8_t *) "Hello, World\r\n", 14);
+		HAL_Delay(1000);
+	}
+
 	return 0;
 }
